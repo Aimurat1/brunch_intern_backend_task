4a26d8670cadc64c34f62ad1f53fafa2c3f1a2bb
Aimurat <aimurat927@gmail.com>
2023-10-27 15:12:35 +0600

    Add proper comments

--- db.py
+++ db.py
@@ -1,13 +1,18 @@
 from pymongo import MongoClient
 import uuid, pymongo
 
+#Connect to the MongoDB remote database
 client = MongoClient("mongodb+srv://admin:wNiMXwTesv42eYcQ@task.dykvv2y.mongodb.net/?tls=true")
 
 db = client.cafe_db
 
+#Input: None
+#Output: randomly generated id
 def generate_id():
     return uuid.uuid4().hex[:20]
 
+#Input: the collection name, id, and dictionary to insert
+#Output: None
 def insert_document_mongo(collection_name, id, dict_to_insert):
     
     dict_to_insert['_id'] = id
@@ -15,6 +20,9 @@ def insert_document_mongo(collection_name, id, dict_to_insert):
     if db[collection_name].find_one({"_id": id}) is None:
         db[collection_name].insert_one(dict_to_insert)
 
+#Input: dictionary containing cafe information
+#Output: calculated total revenue
+#Note: cafe information is inserted to the database
 def insert_cafe(form: dict):
     total_revenue = int(form['number_of_customers']) * float(form['average_spend'])
     
@@ -25,7 +33,9 @@ def insert_cafe(form: dict):
     insert_document_mongo("cafe", generate_id(), form)
     
     return total_revenue
-    
+
+#Input: None
+#Output: last 5 cafe total revenue calculations with total revenue and cafe name
 def get_last_five_cafes():
     
     res = db['cafe'].find({}).sort("datetime", pymongo.DESCENDING).limit(5)
@@ -42,5 +52,4 @@ def get_last_five_cafes():
 
 if __name__ == "__main__":
     
-    # print(get_last_five_cafes())
     pass
--- main.py
+++ main.py
@@ -3,46 +3,53 @@ import db, datetime, pytz
 
 app = Flask(__name__)
 
-@app.route('/new_cafe', methods=['POST', 'GET']) 
+#Input: POST form with "cafe_name", "number_of_customers" and "average_speed" fields
+#Output: calculated total revenue
+#Note: adding endpoint new_cafe to receive POST request with new cafe information
+@app.route('/new_cafe', methods=['POST']) 
 def new_cafe():
     
-    if request.method == "POST":
-        form = request.form.to_dict()
+    form = request.form.to_dict()
+    
+    #Check that all necessary fields in the request, if not return proper error message
+    if "cafe_name" in form.keys() and "number_of_customers" in form.keys() and "average_spend" in form.keys():
+        current_datetime = datetime.datetime.now(tz=pytz.timezone("Asia/Almaty"))
+        current_datetime = current_datetime.replace(tzinfo=None)
         
-        if "cafe_name" in form.keys() and "number_of_customers" in form.keys() and "average_spend" in form.keys():
-            current_datetime = datetime.datetime.now(tz=pytz.timezone("Asia/Almaty"))
-            current_datetime = current_datetime.replace(tzinfo=None)
-            
-            try:
-                number_of_customers = int(form['number_of_customers'])
-            except:
-                return Response(response="Number of customers is not number", status = 400)
-            
-            try:
-                average_spend = float(form['average_spend'])
-            except:
-                return Response(response="Average spend per customer is not number", status = 400)
-            
-            form.update({
-                "number_of_customers": number_of_customers,
-                "average_spend": average_spend,
-                "datetime": current_datetime
-            })
-            
-            total_revenue = db.insert_cafe(form)
-            
-            return jsonify({"total_revenue": total_revenue})
-            
-        else:
-            return Response(response="Missing field(s) in the form", status = 400)
+        #Check that number_of_customers and average_spend are numbers, if not return error message
+        try:
+            number_of_customers = int(form['number_of_customers'])
+        except:
+            return Response(response="Number of customers is not number", status = 400)
+        
+        try:
+            average_spend = float(form['average_spend'])
+        except:
+            return Response(response="Average spend per customer is not number", status = 400)
+        
+        form.update({
+            "number_of_customers": number_of_customers,
+            "average_spend": average_spend,
+            "datetime": current_datetime
+        })
+        
+        #Insert new cafe into database
+        total_revenue = db.insert_cafe(form)
+        
+        #Return Total revenue to the frontend
+        return jsonify({"total_revenue": total_revenue})
         
     else:
-        return Response(status=403)
-    
+        #Return error message
+        return Response(response="Missing field(s) in the form", status = 400)
+        
+#Input: GET request
+#Output: last 5 cafe total revenue calculations with cafe name and total revenue
 @app.route('/last_cafes', methods=['GET']) 
 def last_five_cafes():
     
     if request.method == "GET":
+        #Return last five total revenue calculations
         return jsonify(db.get_last_five_cafes())
 
 
